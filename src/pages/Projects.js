ed231ee0d83eaa9b64db91142bf937a3b1fe16d5
Alison Killen <61318985+alikillen@users.noreply.github.com>
2022-09-15 19:39:28 +1000

    adding projects page

---
+++ src/pages/Projects.js
@@ -0,0 +1,14 @@
+import "../App.css";
+import Footer from "../components/Footer"
+
+function Projects() {
+  return (
+    <>
+      <h1 className='projects'>Projects</h1>
+      <p>some info here about projects I've done</p>
+      <Footer />
+    </>
+  );
+}
+
+export default Projects;
