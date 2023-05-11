d2221cdc8fdae4868fd17c1cd8584f86a947bdae
Alison Killen <61318985+alikillen@users.noreply.github.com>
2023-05-11 22:25:17 +1000

    formatting

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,11 +5,11 @@ import Footer from '../components/Footer'
 function About() {
   return (
     <>
-    <div>
-      <h1 className='about'>About Me</h1>
-      <p>I'm a junior full-stack web developer. I'm on the hunt for a position where I can put my skills to the test and contribute to a team.</p>
-    </div>
-    <p>More about me here - background, hobbies/interests, shecodes, meetups etc. back button</p>
+      <div>
+        <h1 className='about'>About Me</h1>
+        <p>I'm a junior full-stack web developer. I'm on the hunt for a position where I can put my skills to the test and contribute to a team.</p>
+      </div>
+      <p>More about me here - background, hobbies/interests, shecodes, meetups etc. back button</p>
     </>
   )
 }
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -5,12 +5,12 @@ import Footer from "../components/Footer";
 function Resume() {
   return (
     <>
-    <div>
-      <h1 className='resume'>Resume</h1>
-      <p>Read all about it.</p>
+      <div>
+        <h1 className='resume'>Resume</h1>
+        <p>Read all about it.</p>
 
-      <p>there should probably be a pdf resume here...</p>
-    </div>
+        <p>there should probably be a pdf resume here...</p>
+      </div>
     </>
   );
 }
