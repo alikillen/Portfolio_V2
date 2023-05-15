11c3d2b1b3ace7fe61892bf090233b31729ee40a
Alison Killen <61318985+alikillen@users.noreply.github.com>
2023-05-15 23:57:35 +1000

    comment and lint button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,31 +2,33 @@ import React from "react"
 import { Link } from "react-router-dom";
 import "../styles/Button.css";
 
+// todo - why is this styled like this.....fix it
+
 const STYLES = ["btn--primary", "btn--outline"]
 
 const SIZES = ["btn--medium", "btn--large"]
 
 export const Button = ({
-  children, 
-  type, 
-  onClick, 
-  buttonStyle, 
+  children,
+  type,
+  onClick,
+  buttonStyle,
   buttonSize
 }) => {
-  const checkButtonStyle = STYLES.includes(buttonStyle) 
-  ? buttonStyle 
-  : STYLES[0]
+  const checkButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0]
 
   const checkButtonSize = SIZES.includes(buttonSize)
-  ? buttonSize
-  : SIZES[0]
+    ? buttonSize
+    : SIZES[0]
 
   return (
-    <Link to="/contact" className="btn-mobile">
+    <Link to="/projects" className="btn-mobile">
       <button
-      className={`btn ${checkButtonStyle} ${checkButtonSize}`}
-      onClick={onClick}
-      type={type}
+        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+        onClick={onClick}
+        type={type}
       >
         {children}
       </button>
